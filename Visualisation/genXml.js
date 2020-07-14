e4544c4b2cea87b54c11ae630f782d2bcfc598d5
Dan Birks <63508057+DanBirks888@users.noreply.github.com>
2020-07-14 13:10:15 +0100

    Updated XML Functions

--- Visualisation/genXml.js
+++ Visualisation/genXml.js
@@ -17,47 +17,90 @@ function generateXML() {
             // Add Full Article
             output += article;
             // Annotate Reasoning Elements
+
             var reasoning =
                 data[0].keyword_analysis.keyword_analysis.reasoning_86;
             for (a in reasoning) {
-                var re = reasoning[a].marker;
-                output = output.replace(" " + re + " ", "<argumentation type='reasoning'>" + re + "</argumentation>");
+                var reason = reasoning[a].marker;
+                var regex = new RegExp("\\b" + reason, "gmi");
+                var xml_reason = "<argumentation type='reasoning'>" + reason + "</argumentation>"
+                output = output.replace(regex,xml_reason);
             }
+
             // Annotate Personal Experience Elements
             var exp =
                 data[0].keyword_analysis.keyword_analysis.experience_9;
             for (b in exp) {
-                var ex = exp[b].marker;
-                output = output.replace(" " + ex + " ", "<evidence type='experience' sub-type='personal-experience'>" + ex + "</evidence>");
+                var exper = exp[b].marker;
+                if (exper.length == 1) {
+                    var single = exper + " ";
+                    var regex1 = new RegExp("\\b" + single, "gmi");
+                } else {
+                    var regex1 = new RegExp("\\b" + exper, "gmi");
+                }
+                
+                var xml_evidence = "<evidence type='experience' sub-type='personal-experience'>" + exper + "</evidence>"
+                output = output.replace(regex1,xml_evidence);
             }
             // Annotate Time Events
+
             var timex =
                 data[0].event_analysis.event_analysis.timex_events;
             for (c in timex) {
                 var ti = timex[c];
-                output = output.replace(" " + ti + " ", "<evidence type='experience' sub-type='timex-event'>" + ti + "</evidence>");
+                var regex_timex = new RegExp("\\b" + ti, "gmi");
+                var xml_timex = "<evidence type='experience' sub-type='timex-event'>" + ti + "</evidence>"
+                output = output.replace(regex_timex, xml_timex);
             }
+
             // Annotate Verb Events
             var verb =
                 data[0].event_analysis.event_analysis.verb_events;
             for (d in verb) {
                 var ve = verb[d];
-                output = output.replace(" " + ve[0] + " ", "<evidence type='experience' sub-type='verb-event'>" + ve[0] + "</evidence>");
+                var ver = ve[0];
+                if (ver.length > 1) {
+                    var regex_verb = new RegExp("\\b" + ver, "gmi");
+                    var xml_verb = "<evidence type='experience' sub-type='verb-event'>" + ver + "</evidence>"
+                    output = output.replace(regex_verb, xml_verb);
+                }
+                
+              
             }
+
             // Annotate NLTK Named Entities
+
             var nltk =
                 data[0].named_entity_analysis.named_entity_analysis.nltk_named_entities;
             for (e in nltk) {
                 var nl = nltk[e];
-                output = output.replace(" " + nl + " ", "<evidence type='experience' sub-type='named-entity'>" + nl + "</evidence>");
+                var regex_nltk = new RegExp("\\b" + nl, "gmi");
+                var xml_nltk = "<evidence type='experience' sub-type='named-entity'>" + nl + "</evidence>"
+                output = output.replace(regex_nltk, xml_nltk);
             }
+
             // Annotated Pronouns
-            var pronoun =
-                data[0].named_entity_analysis.named_entity_analysis.pronouns;
-            for (f in pronoun) {
-                var pro = pronoun[f];
-                output = output.replace(" " + pro[0] + " ", "<evidence type='experience' sub-type='pronoun'>" + pro[0] + "</evidence>");
-            }
+
+            // var pronoun =
+            //     data[0].named_entity_analysis.named_entity_analysis.pronouns;
+            // for (f in pronoun) {
+            //     var pro = pronoun[f];
+            //     var p = pro[0];
+            //     var regex_pronoun = new RegExp("\\b" + pro[0], "gmi");
+            //     var xml_pronoun = "<evidence type='experience' sub-type='pronoun'>" + pro[0] + "</evidence>"
+            //     output = output.replace(regex_pronoun, xml_pronoun);
+            // }
+
+            // var citations = 
+            //        data[0].citations_analysis.citations_analysis.classified_external_uris;
+            //        for (g in citations) {
+            //            var domain = citations[g].domain;
+            //            var cit = citations[g].classifications[0];
+            //            console.log(cit[g]);
+            //        }
+
+                   
+            
             // End of XML File
             output += "</article>";
             // Show In Console
